001411a26b7efff9889cf3e97a827808b2a4c419
Wyncroft18 <kapulong18@gmail.com>
2025-03-27 04:56:28 +0800

    Finish feature/single-product-page

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function SingleProduct() {
+    const [product, setProduct] = useState([])
+
+    const params = useParams();
+
+    const apiKey = import.meta.env.VITE_API_BASE_URL;
+
+    const php = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "PHP",
+    });
+
+    useEffect(() => {
+        fetch(`${apiKey}/products/${params.productId}`)
+            .then(res => res.json())
+            .then(data => setProduct(data.product))
+    }, [])
+
     return (
         <div className="single-product-container">
             <article className="single-product">
-                <h2>Some product here</h2>
+                <img src={product.imgUrl} alt={product.alt} />
+                <div>
+                    <h2>{product.name}</h2>
+                    <h4>Description:</h4>
+                    <p className="description">{product.description}</p>
+                    <h3>{php.format(product.price)}</h3>
+                    <button className="buy-btn">Buy now</button>
+                </div>
             </article>
         </div>
     );
