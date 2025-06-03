5cd788e279c3b668a51e360ec36f472b2659a154
Wyncroft18 <kapulong18@gmail.com>
2025-06-03 23:01:57 +0800

    Refactor order page for admin users

--- src/pages/Order.jsx
+++ src/pages/Order.jsx
@@ -1,11 +1,15 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState, useContext } from "react";
+import { userContext } from "../App";
 
 export default function Order() {
+    // user context
+    const { user, setUser } = useContext(userContext);
+
     // states
-    const [orders, setOrders] = useState([])
+    const [orders, setOrders] = useState([]);
 
     // api key
-    const apiKey = import.meta.env.VITE_API_BASE_URL
+    const apiKey = import.meta.env.VITE_API_BASE_URL;
 
     // converts number to peso format
     const php = new Intl.NumberFormat("en-US", {
@@ -14,48 +18,75 @@ export default function Order() {
     });
 
     // array of orders
-    const orderElements = orders.map(order => {
-        
-        const productArr = order.productsOrdered.map(product => {
-            return <li key={product.productId}>{product.name} : Quantity {product.quantity}</li>
-        })
+    const orderElements =
+        orders ||
+        [].map((order) => {
+            const productArr = order.productsOrdered.map((product) => {
+                return (
+                    <li key={product.productId}>
+                        {product.name} : Quantity {product.quantity}
+                    </li>
+                );
+            });
 
-        return (
-            <section key={order._id}>
-                <h2>Order placed on: {order.orderedOn.slice(0, 10)}</h2>
-                <h3>Product:</h3>
-                <ul>
-                    {productArr}
-                </ul>
-                <h4>Status: {order.status}</h4>
-                <h3>Total: {php.format(order.totalPrice)}</h3>
-            </section>
-        )
+            return (
+                <section key={order._id}>
+                    <h2>Order placed on: {order.orderedOn.slice(0, 10)}</h2>
+                    <h3>Product:</h3>
+                    <ul>{productArr}</ul>
+                    <h4>Status: {order.status}</h4>
+                    <h3>Total: {php.format(order.totalPrice)}</h3>
+                </section>
+            );
+        });
 
-    })
-
-    // fetch user order or fetch all orders for admin*
+    // effects
     useEffect(() => {
-        fetch(`${apiKey}/orders`, {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem("token")}`
-            }
-        })
-        .then(res => res.json())
-        .then(data => {
-            setOrders(data.userOrders)
-        })
-    }, [])
+        // fetch all orders for admin
+        async function fetchAllOrders() {
+            const response = await fetch(`${apiKey}/orders/all`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+            const data = await response.json();
+            console.log(data);
+            setOrders(data.orders);
+        }
 
-    return (
-        orderElements.length > 0 ?
+        // fetch orders for user
+        async function fetchUserOrders() {
+            const response = await fetch(`${apiKey}/orders`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+            const data = await response.json();
+            console.log(data);
+            setOrders(data.userOrders);
+        }
+
+        user.isAdmin ? fetchAllOrders() : fetchUserOrders();
+
+        // fetch(`${apiKey}/orders`, {
+        //     headers: {
+        //         Authorization: `Bearer ${localStorage.getItem("token")}`,
+        //     },
+        // })
+        //     .then((res) => res.json())
+        //     .then((data) => {
+        //         setOrders(data.userOrders);
+        //     });
+    }, []);
+
+    return orderElements.length > 0 ? (
         <div className="orders-container">
             <h1>Orders</h1>
             {orderElements}
         </div>
-        :
+    ) : (
         <div className="orders-container">
-            <h1>You have no orders</h1>
+            <h1>{user.isAdmin ? "No orders" : "You have no orders"}</h1>
         </div>
-    )
+    );
 }
